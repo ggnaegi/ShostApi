b76d7b9e67f3f9b50e84de052d007b133d4ce004
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2024-11-13 22:19:46 +0100

    try and error with admin redirect

--- Shosta.Functions/API/Authorization.cs
+++ Shosta.Functions/API/Authorization.cs
@@ -14,18 +14,20 @@ public class Authorization(
     private readonly ILogger _logger = loggerFactory.CreateLogger<Organisations>();
 
     [Function(nameof(AdminRedirect))]
-    public IActionResult AdminRedirect(
+    public async Task<HttpResponseData> AdminRedirect(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "auth/admin-redirect")]
         HttpRequestData req,
         FunctionContext executionContext)
     {
         var (authenticated, authorized) = req.IsAuthenticatedAndAuthorized(configuration.GetAdminEmails(), _logger);
+        var redirectUrl = authenticated && authorized 
+            ? configuration.GetValue<string>("SPAUrlAdmin") ?? string.Empty 
+            : configuration.GetValue<string>("SPAUrl") ?? string.Empty;
+        
+        var response = req.CreateResponse(System.Net.HttpStatusCode.Redirect);
+        response.Headers.Add("Location", redirectUrl);
+        response.Headers.Add("Cache-Control", "no-cache");
 
-        if (!authenticated || !authorized)
-        {
-            return new RedirectResult(configuration.GetValue<string>("SPAUrl") ?? string.Empty);
-        }
-
-        return new RedirectResult(configuration.GetValue<string>("SPAUrlAdmin") ?? string.Empty);
+        return response;
     }
 }
