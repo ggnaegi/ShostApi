bfa7ad1f025bc7fcdb6b0a803efb82b558503764
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2024-11-13 23:09:11 +0100

    Trying to handle authorization with jwt token, encrypted in a cookie

--- Shosta.Functions/API/Authorization.cs
+++ Shosta.Functions/API/Authorization.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Shosta.Functions.Auth;
 
 namespace Shosta.Functions.API;
@@ -19,15 +22,45 @@ public class Authorization(
         HttpRequestData req,
         FunctionContext executionContext)
     {
-        var (authenticated, authorized) = req.IsAuthenticatedAndAuthorized(configuration.GetAdminEmails(), _logger);
-        var redirectUrl = authenticated && authorized 
-            ? configuration.GetValue<string>("SPAUrlAdmin") ?? string.Empty 
-            : configuration.GetValue<string>("SPAUrl") ?? string.Empty;
-        
+        var claimsPrincipal = req.GetClaimsPrincipalFromRequest(_logger);
+        if (claimsPrincipal?.Identity is not { IsAuthenticated: true })
+        {
+            return req.CreateResponse(System.Net.HttpStatusCode.Unauthorized);
+        }
+
+        var token = GenerateJwtToken(claimsPrincipal.Claims);
+
         var response = req.CreateResponse(System.Net.HttpStatusCode.Redirect);
+        response.Headers.Add("Set-Cookie", $"auth_token={token}; HttpOnly; Secure; Path=/; SameSite=Strict");
+
+        var redirectUrl = IsAdminUser(claimsPrincipal)
+            ? configuration.GetValue<string>("SPAUrlAdmin")
+            : configuration.GetValue<string>("SPAUrl");
         response.Headers.Add("Location", redirectUrl);
-        response.Headers.Add("Cache-Control", "no-cache");
 
         return response;
+    }
+
+    private string GenerateJwtToken(IEnumerable<Claim> claims)
+    {
+        var secretKey = configuration.GetValue<string>("JwtSecretKey") ??
+                        throw new Exception("JWT Secret Key is missing");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(1),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    private bool IsAdminUser(ClaimsPrincipal claimsPrincipal)
+    {
+        var email = claimsPrincipal.FindFirst(ClaimTypes.Email)?.Value;
+        var adminEmails = configuration.GetSection("AdminEmails").Get<string[]>();
+        return adminEmails?.Length > 0 && adminEmails.Contains(email);
     }
 }
